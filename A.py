26e880a7bf2db3ce2652b524da204574ca04e882
Ismael-Safadi <31775833+Ismael-Safadi@users.noreply.github.com>
2018-01-03 19:25:44 +0200

    Add files via upload

---
+++ A.py
@@ -0,0 +1,26 @@
+from hashlib import md5
+from base64 import b64decode
+from base64 import b64encode
+from Crypto import Random
+from Crypto.Cipher import AES
+from tkinter import messagebox
+BLOCK_SIZE = 16  
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * \
+                chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+class AESCipher:
+    def __init__(self, key):
+        self.key = md5(str(key)).hexdigest()
+
+    def encrypt(self, raw):
+        raw = pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return unpad(cipher.decrypt(enc[16:])).decode('utf8')
